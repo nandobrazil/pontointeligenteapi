544bb880e1b039f26969344f5e589e100da43223
nandobrazil <contato@frinfo.com.br>
2021-10-13 00:06:20 -0400

    Criação do projeto Ponto Eletrônico Inteligente

---
+++ src/main/java/br/com/frinfo/pontointeligente/PontointeligenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.frinfo.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PontointeligenteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/frinfo/pontointeligente/PontointeligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frinfo.pontointeligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
